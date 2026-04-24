22aadbd6b9b80d024e77c1be8b1c8a2a1f8514fd
juanemilioestevesdelgado-hue <juanemilioestevesdelgado@gmail.com>
2026-04-23 22:20:10 -0500

    FORZAR ACTUALIZACION DE DATOS - V3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { fullInventory } from './data.js?v=2';
+import { fullInventory } from './data.js?v=3';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {
